c3e160b52d25843c88995cf04de7ab29b56a80d8
cjohnson1341 <33328629+cjohnson1341@users.noreply.github.com>
2018-05-26 12:18:07 -0500

    Changes on 5.26

--- script.js
+++ script.js
@@ -2,14 +2,21 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var mybutton=document.getElementById('mybutton');
 
 function setGradient() {
 	body.style.background = "linear-gradient(to right," + color1.value + "," + color2.value + ")";
-	css.textContent = body.style.background + ";";
+	css.innerHTML = body.style.background + ";";
 }
-
 
 
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+document.addEventListener("DOMContentLoaded", setGradient );
+
+
+
+
+
